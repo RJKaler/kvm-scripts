5841813e98fd0e68005cb668025c2048be724310
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2025-07-30 05:12:37 -0400

    Create remove-snapshots.sh

---
+++ remove-snapshots.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+vm_name="$1"
+
+script_name="$(basename "$0")"
+
+if [[ "$vm_name" =~ ^(-h|--h|help|Help|h)$ ]]; then
+    echo "Enter a domain to remove all snapshots"
+    echo "Ex): $script_name [ubuntu-server]"
+    exit 0
+fi
+
+
+if [[ -z "$vm_name" ]]; then
+    echo "Error - an arg is required"
+    echo "Enter a domain"
+    echo "Ex): $script_name [ubuntu-server]"
+    exit 1
+fi
+
+ virsh snapshot-list "$vm_name" | grep -iEv '\--|Name|^$' | awk '{ print $1 }' \
+| while IFS= read -r snap_name
+do
+    sudo virsh snapshot-delete "$vm_name" "$snap_name" || error
+done
+
+read -rep "Would you like to remove the host? (y|n) " ans
+
+    if [[ "$ans" =~ ^(y|Y|yes|Yes)$ ]]; then
+        sudo virsh undefine "$vm_name" --remove-all-storage || error
+        echo "Successfully removed $vm_name"
+        exit 0
+    else
+        echo "Ok - exiting script..."
+        exit 0
+    fi
+
